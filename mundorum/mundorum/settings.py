50568776ab3deefcfdde419e9191a194c2df01b7
gigennari <giov.gennari@hotmail.com>
2025-06-09 22:54:06 -0300

    cria schema atual

--- mundorum/mundorum/settings.py
+++ mundorum/mundorum/settings.py
@@ -51,7 +51,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders'
+    'corsheaders',
+    'django_extensions'
+
 ]
 
 MIDDLEWARE = [
